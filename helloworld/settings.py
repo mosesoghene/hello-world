d62f604fb06f7e3bfef197b927872d3c29bb4b02
Moses Oghene <mosesogheneo@gmail.com>
2022-07-03 13:06:00 +0100

    Update for heroku deployment

--- helloworld/settings.py
+++ helloworld/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '#h(5r5s$8*)(_ta%3g^5@jf*d0-w2@(qt42gt^wa+ueupr!!wb'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
